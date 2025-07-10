83b0fbcd4490d505f6934c49149a846893a98a29
Alan Bañuelos <alan_blk@live.com>
2025-07-10 15:03:54 -0600

    Arreglo del código y adición de funciones adicionales

--- src/com/generation/Codigo4.java
+++ src/com/generation/Codigo4.java
@@ -2,38 +2,68 @@ package com.generation;
 import java.util.Scanner;
 //Se importa funciön de Scanner
 
+
 public class Codigo4 {
-    Scanner scan = new Scanner();
+    // Se añade PSVM
+    public static void main(String[] args) {
 
-    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-        String j1 = scan.nextLine();
+        Scanner scan = new Scanner(System.in);
+        String j1;
+        String j2;
+        //Se agregan variables j1 y 2 de manera previa para habilitar el uso de bucles
 
-    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-//        Scanner s2 = new Scanner(); Se elimina escaner sin usar
-        String j2 = scan.nextLine();
-    // Se elimina ) sobrante
-    if (j1 == j2) {
-        System.out.println("Empate");
-    } else {
-int g = 2;
-        switch(j1) {
-        case "piedra":
-        if (j2 == "tijeras") {
-g = 1;
+        // Se añaden bucles while para evaluar resultados no válidos
+        do {
+            System.out.println("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
+            j1 = scan.nextLine().toLowerCase();
+            if (!j1.equals("piedra")&&!j1.equals("papel")&&!j1.equals("tijeras")){
+                System.out.println("Opción inválida, escribe piedra, papel o tijeras");
+            }
+            // Se añade toLowerCase para evitar problemas con mayus
+        }while(!j1.equals("piedra")&&!j1.equals("papel")&&!j1.equals("tijeras"));
+
+
+        do {
+            // Se corrige turno del jugador 2
+            System.out.println("Turno del jugador 2 (introduzca piedra, papel o tijeras): ");
+            //Scanner s2 = new Scanner(); Se elimina escáner sin usar
+            j2 = scan.nextLine().toLowerCase();
+            if (!j2.equals("piedra")&&!j2.equals("papel")&&!j2.equals("tijeras")) {
+                System.out.println("Opción inválida, escribe piedra, papel o tijeras");
+            }
+        }while(!j2.equals("piedra")&&!j2.equals("papel")&&!j2.equals("tijeras"));
+
+        System.out.println("Turno del jugador 2 (introduzca piedra, papel o tijeras): ");
+
+
+        // Se rehace la estructura para comparar los inputs
+        // Se elimina ")" sobrante y se sustituye "==" por ".equals"
+        if (j1.equals(j2)) {
+            System.out.println("Empate");
+        } else {
+        switch (j1) {
+                case "piedra":
+                    if (j2.equals("tijeras")) {
+                        System.out.println("Ha ganado el primer jugador");
+                    } else {
+                        System.out.println("Ha ganado el segundo jugador");
+                    }
+                    break;
+                case "tijeras":
+                    if (j2.equals("piedra")){
+                        System.out.println("Ha ganado el segundo jugador");
+                    } else {
+                        System.out.println("Ha ganado el segundo jugador");
+                    }
+                    break;
+                case "papel":
+                    if (j2.equals("tijeras")){
+                        System.out.println("Ha ganado el segundo jugador");
+                    } else {
+                        System.out.println("Ha ganado el primer jugador");
+                    }
+                    break;
         }
-
-        case "papel":
-        if (j2 == "piedra") {
-g = 1;
-
-        case "tijera":
-        if (j2.equals("papel")) {
-g = 1;
-        }
-        break;
-default:
-        }
-        System.out.println("Gana el jugador " + g);
         }
     }
 }
